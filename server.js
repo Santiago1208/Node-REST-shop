b6beeaa99ed90590dc675cf7e93d6a3b2d0849ee
Santiago Restrepo Prado <sanpra1208@gmail.com>
2020-01-21 09:22:15 -0500

    1. Project setup

--- server.js
+++ server.js
@@ -2,10 +2,11 @@
 
 // This instruction is the homonime of import x from y.
 const http = require('http')
+const app = require('./app')
 
 // Port adquired from environment variable. Another way is hardcoding the port.
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 // Creates a listener to listen request from internet
-const server = http.createServer()
+const server = http.createServer(app)
 server.listen(port)
