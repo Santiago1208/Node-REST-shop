c8d9d6dc0c104df2dfe959c6c532b50fe5d120f6
Santiago Restrepo Prado <sanpra1208@gmail.com>
2020-01-21 17:28:57 -0500

    7. Add Orders management

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,28 +1,74 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+
+const Order = require('../models/order')
+const Product = require('../models/product')
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Orders were fetched'
+    Order.find().select('-__v').exec().then(result => {
+        if (result) {
+            res.status(200).json({
+                count: result.length,
+                orders: result
+            })
+        } else {
+            res.status(404).json({
+                message: 'No orders saved yet'
+            })
+        }
+    }).catch(error => {
+        res.status(500).json({
+            error: error
+        })
     })
+    
 })
 
 router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.productQuantity
-    }
-    res.status(200).json({
-        message: 'Order was created',
-        createdOrder: order
+    const productId = req.body.productId
+    Product.findOne(productId).select('_id').exec().then(result => {
+        const order = new Order({
+            _id: new mongoose.Types.ObjectId(),
+            productId: productId,
+            quantity: req.body.productQuantity
+        })
+        order.save().then(result =>{
+            res.status(201).json({
+                message: 'Order was created',
+                createdOrder: result
+            })
+        }).catch(error => {
+            res.status(500).json({
+                error: error
+            })
+        })
+    }).catch(error => {
+        res.status(500).json({
+            error: error
+        })
     })
+    
 })
 
 router.get('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Order details',
-        orderId: req.params.orderId
+    const orderId = req.params.orderId
+    Order.findById(orderId).select('-__v').exec().then(result => {
+        if (result) {
+            res.status(200).json({
+                order: result
+            })
+        } else {
+            res.status(404).json({
+                message: 'No order saved with the ID ' + orderId
+            })
+        }
+    }).catch(error => {
+        res.status(500).json({
+            error: error
+        })
     })
+    
 })
 
 // Allows to import the products file in app.js
